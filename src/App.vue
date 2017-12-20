8cfcc29998bd34149a9492ccf13a358f9735d63d
Chris-Wen <whx555666@163.com>
2017-12-20 22:29:17 +0800

    引入scss

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -21,3 +21,6 @@ export default {
   margin-top: 60px;
 }
 </style>
+
+
+

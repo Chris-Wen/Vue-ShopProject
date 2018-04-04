e1303d04bb239c9f9b71f2cfdb102339b7fb5535
Wen-C <whx555666@163.com>
2018-04-04 18:03:21 +0800

    2018/4/4

--- src/components/SliderDelete.vue
+++ src/components/SliderDelete.vue
@@ -1,36 +1,32 @@
 <template>
-  <div class="delete">
-        <div class="slider">
-            <div class="content"  @touchstart='touchStart' @touchmove='touchMove' @touchend='touchEnd' :style="deleteSlider">
-        <!-- 插槽中放具体项目中需要内容         -->   
-            <slot>
-
-            </slot>
-            </div>
-            <div class="remove" ref='remove'>删除</div>
+<div class="delete">
+    <div class="slider" @touchstart='touchStart' @touchmove='touchMove' @touchend='touchEnd'>
+        <div class="content"  :style="deleteSlider">
+    <!-- 插槽中放具体项目中需要内容         -->   
+        <slot name="content"> </slot>
         </div>
+        <div class="remove" ref='remove' > <span>删除</span> </div>
     </div>
+</div>
 </template>
 
 <script>
 export default {
     name: 'SliderDelete',
     data() {
-    return {
-        startX:0,   //触摸位置
-        endX:0,     //结束位置
-        moveX: 0,   //滑动时的位置
-        disX: 0,    //移动距离
-        deleteSlider: '',//滑动时的效果,使用v-bind:style="deleteSlider"
-    }
-
+        return {
+            startX:0,   //触摸位置
+            endX:0,     //结束位置
+            moveX: 0,   //滑动时的位置
+            disX: 0,    //移动距离
+            deleteSlider: '',//滑动时的效果,使用v-bind:style="deleteSlider"
+        }
     },
     methods:{
         touchStart(ev){
             ev= ev || event
-        //tounches类数组，等于1时表示此时有只有一只手指在触摸屏幕
-
-        if(ev.touches.length == 1){
+            //tounches类数组，等于1时表示此时有只有一只手指在触摸屏幕
+            if (ev.touches.length == 1) {
                 // 记录开始位置
                 this.startX = ev.touches[0].clientX;
             }
@@ -41,22 +37,23 @@ export default {
             let wd=this.$refs.remove.offsetWidth;
                 if(ev.touches.length == 1) {
                     // 滑动时距离浏览器左侧实时距离
+                    console.log(ev.touches[0])
                     this.moveX = ev.touches[0].clientX
             
                     //起始位置减去 实时的滑动的距离，得到手指实时偏移距离
                     this.disX = this.startX - this.moveX;
-                console.log(this.disX)
+                    // console.log(this.disX)
                     // 如果是向右滑动或者不滑动，不改变滑块的位置
                     if(this.disX < 0 || this.disX == 0) {
-                        this.deleteSlider = "transform:translateX(0px)";
+                        this.deleteSlider = "marginLeft:0px";
                     // 大于0，表示左滑了，此时滑块开始滑动 
                     }else if (this.disX > 0) {
                             //具体滑动距离我取的是 手指偏移距离*5。
-                        this.deleteSlider = "transform:translateX(-" + this.disX*5 + "px)";
+                        this.deleteSlider = "marginLeft:-" + this.disX + "px";
                         
                         // 最大也只能等于删除按钮宽度 
                         if (this.disX*5 >=wd) {
-                            this.deleteSlider = "transform:translateX(-" +wd+ "px)";
+                            this.deleteSlider = "marginLeft:-" +wd+ "px";
                             
                         }
                     }
@@ -67,17 +64,17 @@ export default {
             let wd=this.$refs.remove.offsetWidth;
             if (ev.changedTouches.length == 1) {
                 let endX = ev.changedTouches[0].clientX;
-                    
+
                     this.disX = this.startX - endX;
                     console.log(this.disX)
                     //如果距离小于删除按钮一半,强行回到起点
                     
                     if ((this.disX*5) < (wd/2)) {
                         
-                        this.deleteSlider = "transform:translateX(0px)";
+                        this.deleteSlider = "marginLeft:0px";
                     }else{
                         //大于一半 滑动到最大值
-                            this.deleteSlider = "transform:translateX(-"+wd+ "px)";
+                            this.deleteSlider = "marginLeft:-"+wd+ "px";
                     }
                 }
             }      
@@ -89,35 +86,54 @@ export default {
 
 
 <style lang="scss" scoped>
+.delete{
+    width: 100%;
+    height: auto;
+    overflow: hidden;
+    -webkit-box-sizing: border-box;
+            box-sizing: border-box;
     .slider{
-        width: 100%;
+        width: 130%;
         height:200px;
-        position: relative;
-         user-select: none;
+        display: -webkit-box;
+        display: -ms-flexbox;
+        display: flex;
+        // -webkit-box-align: center;
+        //     -ms-flex-align: center;
+        //         align-items: center;
+        -webkit-box-orient: horizontal;
+        -webkit-box-direction: normal;
+            -ms-flex-flow: row nowrap;
+                flex-flow: row nowrap;
+        -webkit-transition: 0.3s;
+            transition: 0.3s;        
+        // margin-left: -30%;
         .content{
-            position: absolute;
-            left: 0;
-            right: 0;
-            top: 0;
-            bottom: 0;
-            background:green;
-            z-index: 100;
-            //    设置过渡动画
-            transition: 0.3s;
-             
+            display: inline-block;
+            width: 100%;
+            height: 100%;
+            -webkit-box-sizing: border-box;
+                    box-sizing: border-box; 
+            background-color: green;
         }
         .remove{
-            position: absolute;
-            width:200px;
-            height:200px;
+            display: inline-block;
+            width: 30%;
+            height:100%;
             background:red;
-            right: 0;
-            top: 0;
             color:#fff;
             text-align: center;
-            font-size: 40px;
-            line-height: 200px;
+            font-size: 1.5em;
+            position: relative;
+            span {
+                position: absolute;
+                top: 45%;
+                bottom: 0;
+                right: 0;
+                left: 0;
+            }
         }
     }
+}
 </style>
 
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,11 +1,90 @@
 <template>
     <div class="container">
         <banner id="banner"/>
+        <!-- 积分 -->
+        <div class="score">
+            <p class="title">
+                <span>我的积分</span> <i>{{this.$store.state.userInfo.score || '***'}}</i> 
+            </p>
+            <ul class="score-bar flex-around">
+                <router-link tag="li" to="/points">  <i></i>  <p>赚积分</p>    </router-link >
+                <router-link tag="li" to="/draw">    <i></i>  <p>积分抽奖</p>  </router-link >
+                <router-link tag="li" to="/order">    <i></i> <p>我的订单</p>  </router-link >
+                <router-link tag="li" to="/address">  <i></i> <p>地址管理</p>  </router-link >
+            </ul>
+        </div>
+
+        <i class="pic-background recommend"></i>
+        <div class="link-card flex-between">
+            <div class="card-list">
+                <ul class="flex-between score_goods">
+                    <router-link tag="li" :to="{ path: 'list', query: { type: 1 }}">
+                        <a > <i style="background-position: -12.298039rem 0;"></i> </a>
+                        <p>5000分以下</p>
+                    </router-link>
+                    <router-link tag="li" :to="{ path: 'list', query: { type: 2 }}">
+                        <a > <i style="background-position: -10.462745rem 0;"></i> </a>
+                        <p>5000-8000分</p>
+                    </router-link>
+                    <router-link tag="li" :to="{ path: 'list', query: { type: 3 }}">
+                        <a > <i style="background-position: -8.878431rem 0;"></i> </a>
+                        <p>8000分以上</p>
+                    </router-link>
+                </ul>
+                <p class="dotted"> {{'.....'.repeat(20)}} </p>
+                <ul class="flex-between other_goods">
+                    <li>
+                        <a href="javascript:;"> <i style="background-position: -17.615686rem 0;"></i> </a>
+                        <p>新品专区</p>
+                    </li>
+                    <li>
+                        <a href="javascript:;"> <i style="background-position: -14.054902rem 0;"></i> </a>
+                        <p>好货精选</p>
+                    </li>
+                    <li>
+                        <a href="javascript:;"> <i style="background-position: -15.843137rem 0;"></i> </a>
+                        <p>淘优惠</p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <!-- 商品分类 -->
+        <i class="pic-background"></i>
+        <div class="link-card flex-between goods-sort">
+            <div class="card-list">
+                <ul class="flex-between sport">
+                    <li>
+                        <a href="javascript:;"> <i style=""></i> </a>
+                        <p>美食生活</p>
+                    </li>
+                    <li>
+                        <a href="javascript:;"> <i style=""></i> </a>
+                        <p>家居百货</p>
+                    </li>
+                    <li>
+                        <a href="javascript:;"> <i style=""></i> </a>
+                        <p>数码运动</p>
+                    </li>
+                </ul>
+                <p class="dotted">{{ '····'.repeat(20) }} </p>
+                <ul class="flex-between disport">
+                    <li>
+                        <a href="javascript:;"> <i style=""></i> </a>
+                        <p>动漫卡通</p>
+                    </li>
+                    <li>
+                        <a href="javascript:;"> <i style=""></i> </a>
+                        <p>美妆时尚</p>
+                    </li>
+                    <li>
+                        <a href="javascript:;"> <i style=""></i> </a>
+                        <p>电子卡券</p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
         <ul>
-            <li>这是主页</li>
-            <li>这是主页</li>
-            <li>这是主页</li>
-            <li>这是主页</li>
             <li>这是主页</li>
             <li>这是主页</li>
         </ul>
@@ -53,8 +132,115 @@ export default {
 
 <style lang="scss" scoped>
 .container{
-    box-sizing: border-box;
-    width: 100%;
+    .score {
+        width: 100%;
+        min-height: 3.482133rem;
+        background-color: white;
+        padding-top: .461361rem;
+        .title {
+            width: 100%;
+            text-align: center;
+            font-size:14px;
+            margin-bottom: .276817rem;
+            position: relative;   
+            span,i {
+                position: relative;
+                display: inline-block;
+                -webkit-box-sizing: border-box;
+                        box-sizing: border-box;
+                text-align: center;
+                height: .54902rem;
+                line-height: .54902rem;
+                width: 2.306805rem;
+            }   
+            span {
+                background-color: #ff993e;
+                border-top-left-radius: .461361rem;
+                border-bottom-left-radius: .461361rem;
+                color:white;
+                padding: 1px 0;
+            }      
+            i {
+                color:#ffa970;
+                border:1px solid #ff993e;
+                border-top-right-radius: .461361rem;
+                border-bottom-right-radius: .461361rem;
+            }
+        }
+        .score-bar { 
+            width: 100%; color:#8d8d8d; 
+            li {
+                width: 18%;
+                height: auto;
+                text-align: center;
+                padding: .133333rem;
+                i{
+                    background: url("../assets/images/img.png")  no-repeat;
+                    background-size:19.173333rem 2.666667rem;
+                    display: inline-block;
+                    height:1.5rem;
+                    width: 1.5rem;
+                }
+            }
+            li:first-child i{  background-position:0 -1.223467rem; }
+            li:nth-child(2) i{ background-position:-1.646933rem -1.223467rem; }
+            li:nth-child(3) i{ background-position:-3.262747rem -1.223467rem; }
+            li:last-child i{ background-position:-4.941067rem -1.223467rem; }
+        }
+    }
+    .pic-background { 
+        display: inline-block;
+        width: 100%;
+        min-height: 2.3rem;
+        background: url("../assets/images/sort.jpg") no-repeat;
+        background-size:100%; 
+    }
+    .recommend { 
+        background: url("../assets/images/recommend.jpg") no-repeat;
+        background-size:100%;
+    } 
+
+    //商品推荐， 分类
+    .link-card {
+        width: 100%;
+        height: auto;
+        padding:0.1rem 0;
+        margin-bottom:	.470588rem; 
+        background: #fff;
+        .card-list {
+            width: 100%;
+            min-height: 5rem;
+            padding: 0 .470588rem;
+            -webkit-box-sizing: border-box;
+                    box-sizing: border-box;
+            ul {
+                width: 100%;
+                min-height: 3rem;
+                -webkit-box-sizing:border-box;
+                        box-sizing:border-box;
+                li{
+                    display: block;
+                    width: 33%;
+                    height: auto;
+                    a i{
+                        background:url("../assets/images/img.png")  no-repeat;
+                        background-size:19.173333rem 2.666667rem;
+                        width: 1.6rem;
+                        height: 1.647059rem;
+                        display: block;
+                        margin: 0 auto;
+                        margin-top: .313725rem;
+                    }
+                }
+            }
+        }
+    }
+    .goods-sort {
+        background:url("../assets/images/img.png")  no-repeat;
+        background-size:24.46rem 3.4rem;
+        width: 1.45rem;
+        height: 1.5rem;
+    }
 }
 
 </style>
